fd82755080f71f084f646050531c5e117eca25e0
ZhouChunyang <2609714703@qq.com>
2022-06-13 10:01:51 +0800

    Initial commit

---
+++ src/main/java/com/banmuye/woodrpc/WoodRpcApplication.java
@@ -0,0 +1,13 @@
+package com.banmuye.woodrpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoodRpcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WoodRpcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/banmuye/woodrpc/WoodRpcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banmuye.woodrpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoodRpcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
